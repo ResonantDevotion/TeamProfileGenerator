792d06846b7fa76624e4706ecf024acf14a0891e
rebecca wood <rebeccaseverowood@gmail.com>
2023-03-08 11:16:36 +1300

    Downgraded inquirer, added variables for team members(TM) and TM functions

--- challenge/starter/index.js
+++ challenge/starter/index.js
@@ -10,6 +10,66 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+const engineer = [{
+    type: 'input',
+    message: 'What is the engineers name?',
+    name: 'eName'
+},
+{
+    type: 'input',
+    message: 'What is the engineers ID?',
+    name: 'eID'
+},
+{
+    type: 'input',
+    message: 'What is the engineers email?',
+    name: 'eEmail'
+},
+{
+    type: 'input',
+    message: 'What is the engineers github username?',
+    name: 'eGithub'
+}];
+
+const intern = [{
+    type: 'input',
+    message: 'What is the interns name?',
+    name: 'iName'
+},
+{
+    type: 'input',
+    message: 'What is the interns ID?',
+    name: 'iID'
+},
+{
+    type: 'input',
+    message: 'What is the interns email?',
+    name: 'iEmail'
+},
+{
+    type: 'input',
+    message: 'What is the interns school?',
+    name: 'iSchool'
+}];
+
+const addTeamMember =
+{
+    type: 'list',
+    name: 'more',
+    message: 'Do you want to:',
+    choices: ['Add an engineer', 'Add an intern', 'Finish building the team']
+};
+
+
+const teamMemberChoice =  (response) => {
+    const choices = ['Add an engineer', 'Add an intern', 'Finish building the team'];
+    if (response.more === choices[0]) {
+        return engineer;
+    } else if (response.more === choices[1]){
+        return intern;
+    } else {
+        return
+    }};
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
@@ -34,72 +94,33 @@ inquirer.prompt([
         message: 'What is your team managers office number?',
         name: 'mOffice'
     },
-
-    {
-        type: 'menu',
-        message: 'What is your team managers email address?',
-        choices: ['Add an engineer', 'Add an intern', 'Finish building the team']
-    },
-
-
+    addTeamMember
 ])
 .then((response) => {
-    // create a variable to pull the global scope function into this promise
-    
-    // if there isnt already have a file, it will create one with the following
-    fs.writeFile(
-        // this is the file to be created
-        `team.html`,
-        // this is what will be in the file (pulls the previously mentioned variable/function)
-        
-        // this is a validation, if there is an error it will console else, success will be consoled.
-        (err) => err ? console.error(err) : console.log('Success')
-    )
+    teamMemberChoice(response)
 })
 
 
-// {
-//     type: 'input',
-//     message: 'What is the engineers name?',
-//     name: 'eName'
-// },
-// {
-//     type: 'input',
-//     message: 'What is the engineers ID?',
-//     name: 'eID'
-// },
-// {
-//     type: 'input',
-//     message: 'What is the engineers email?',
-//     name: 'eEmail'
-// },
-// {
-//     type: 'input',
-//     message: 'What is the engineers github username?',
-//     name: 'eGithub'
-// },
 
-// // intern
-// {
-//     type: 'input',
-//     message: 'What is the interns name?',
-//     name: 'iName'
-// },
-// {
-//     type: 'input',
-//     message: 'What is the interns ID?',
-//     name: 'iID'
-// },
-// {
-//     type: 'input',
-//     message: 'What is the interns email?',
-//     name: 'iEmail'
-// },
-// {
-//     type: 'input',
-//     message: 'What is the interns school?',
-//     name: 'iSchool'
-// },
+// .then((response) => {
+//     // create a variable to pull the global scope function into this promise
+    
+//     // if there isnt already have a file, it will create one with the following
+//     fs.writeFile(
+//         // this is the file to be created
+//         `team.html`,
+//         // this is what will be in the file (pulls the previously mentioned variable/function)
+//         response.render,
+//         // this is a validation, if there is an error it will console else, success will be consoled.
+//         (err) => err ? console.error(err) : console.log('Success')
+//     )
+// })
+
+
+
+
+
+
 
 
 // render(
