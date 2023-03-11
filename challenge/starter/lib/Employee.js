ee4eb36174784f0023f576546918d6ea4cce467e
rebecca wood <rebeccaseverowood@gmail.com>
2023-03-11 13:23:30 +1300

    added last comments

--- challenge/starter/lib/Employee.js
+++ challenge/starter/lib/Employee.js
@@ -1,28 +1,30 @@
 // TODO: Write code to define and export the Employee class
+//class constructor function called employee this is the parent class
 class Employee {
-    // housing primitive data types
-    constructor(name, id, email){
+    // constructor which houses primitive data types, and is a blueprint which data will be attributed to
+    constructor(name, id, email) {
+        // assigning the primative data type blueprint to a this.
         this.name = name;
         this.id = id;
         this.email = email;
     };
-
-    getName(){
+    // function to return the data that was attributed to the primative data type using the this. tag
+    getName() {
         return this.name;
     };
-
-    getId(){
+    // function to return the data that was attributed to the primative data type using the this. tag
+    getId() {
         return this.id;
     };
-
-    getEmail(){
+    // function to return the data that was attributed to the primative data type using the this. tag
+    getEmail() {
         return this.email
     };
-
-    getRole(){
+    // function to return a string
+    getRole() {
         return 'Employee'
     };
-
 };
 
+// exports the internal .js to be used externally
 module.exports = Employee;
--- challenge/starter/lib/Intern.js
+++ challenge/starter/lib/Intern.js
@@ -1,19 +1,24 @@
+// variable that pulls in the parent class constructor
 const Employee = require("./Employee");
 
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+// syntax that allows the following child class constructor to use the parent as a base.
 class Intern extends Employee {
-    constructor(name, id, email, school){
+    // constructor which houses primitive data types, and is a blueprint which data will be attributed to
+    constructor(name, id, email, school) {
+        // super is syntax to pull in from parent class
         super(name, id, email);
+        // this is an additional assignment particular to this constructor
         this.school = school;
     };
-
-    getRole(){
+    // similar function to the parent but is altered to display a different string anmd so is stated. Other functions are not repeated unless changed.
+    getRole() {
         return 'Intern';
     };
-
-    getSchool(){
+    // additional function to return the data that was attributed to the primative data type using the this. tag
+    getSchool() {
         return this.school;
     };
 }
-
+// exports the internal .js to be used externally
 module.exports = Intern;
